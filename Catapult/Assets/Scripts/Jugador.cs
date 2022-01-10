5ba0dd427fb9e0e3521ea132beaeeeef02d1dc11
Gonzalo Estrada Rojo <48635918+gongoronzo@users.noreply.github.com>
2022-01-10 01:38:56 +0100

    Logica de plataformas actualizada
    Raycast logic quitada.
    Colliders cambiados para la colisión horizontal.
    Lógica para mover jugadores y puntos de ultimo jugador añadida

--- Catapult/Assets/Scripts/Jugador.cs
+++ Catapult/Assets/Scripts/Jugador.cs
@@ -15,67 +15,102 @@ using UnityEngine.UI;
 public class Jugador : MonoBehaviour
 {
 
-    public float platformMovSpeed = 1f;
+    public float platformMovSpeed;
     public GameObject JugadorPrefab;
     [SerializeField] LayerMask capaPlataforma;
 
     private Rigidbody2D rb;
+    private bool moverEnPlataforma = false;
+    private GameObject plataformaChocada;
+    private GameObject currentLastJugador;
+    private List<GameObject> currentListPlat;
+    private List<GameObject> ListJugPlatAlta = new List<GameObject>();
+    private List<GameObject> ListJugPlatMedia = new List<GameObject>();
+    private List<GameObject> ListJugPlatBaja = new List<GameObject>();
 
     private void Start()
     {
+        platformMovSpeed = 0.5f;
         rb = GetComponent<Rigidbody2D>();
+        Debug.DrawRay( transform.position - new Vector3(transform.localScale.x / 2, transform.localScale.y / 2, 0), Vector3.down*0.15f, Color.red);
+        Debug.DrawRay( transform.position - new Vector3(-transform.localScale.x / 2, transform.localScale.y / 2, 0), Vector3.down*0.15f, Color.green);
     }
 
     private void Update()
     {
-        //Raycast empezando desde los pies en la esquina izq y derecha
-        RaycastHit2D rayCastDer = Physics2D.Raycast(transform.position - new Vector3(transform.localScale.x / 2, transform.localScale.y / 2, 0), Vector2.down,0.15f, capaPlataforma);
-        RaycastHit2D rayCastIzq = Physics2D.Raycast(transform.position - new Vector3(-transform.localScale.x / 2, transform.localScale.y / 2, 0), Vector2.down,0.15f, capaPlataforma);
-        
-        Debug.DrawRay( transform.position - new Vector3(transform.localScale.x / 2, transform.localScale.y / 2, 0), Vector3.down*0.15f, Color.red);
-        Debug.DrawRay( transform.position - new Vector3(-transform.localScale.x / 2, transform.localScale.y / 2, 0), Vector3.down*0.15f, Color.green);
-        
-        //Si alguna esquina choca con la plataforma, se crea el nuevo jugador en la plataforma
-        if(rayCastIzq || rayCastDer)
-        {
-            Debug.Log("RayCastDer collider " + rayCastDer.collider);
-            Debug.Log("RayCastIzq collider " + rayCastIzq.collider);
-            GameObject plataformaChocada = rayCastIzq ? rayCastIzq.collider.gameObject : rayCastDer.collider.gameObject;
-            GameObject lastJug = plataformaChocada.transform.Find("Last Jugador " + PlataformaColisionada(plataformaChocada.name)).gameObject;
+        //RaycastHit2D rayCastDer = Physics2D.Raycast(transform.position - new Vector3(transform.localScale.x / 2, transform.localScale.y / 2, 0), Vector2.down,0.15f, capaPlataforma);
+        //RaycastHit2D rayCastIzq = Physics2D.Raycast(transform.position - new Vector3(-transform.localScale.x / 2, transform.localScale.y / 2, 0), Vector2.down,0.15f, capaPlataforma);
+        ////Raycast empezando desde los pies en la esquina izq y derecha
+        //Debug.DrawRay( transform.position - new Vector3(transform.localScale.x / 2, transform.localScale.y / 2, 0), Vector3.down*0.15f, Color.red);
+        //Debug.DrawRay( transform.position - new Vector3(-transform.localScale.x / 2, transform.localScale.y / 2, 0), Vector3.down*0.15f, Color.green);
+        //GameObject plataformaChocada = null;
+        ////GameObject newJugador = null;
+        ////Si alguna esquina choca con la plataforma, se crea el nuevo jugador en la plataforma
+        //if (rayCastIzq || rayCastDer)
+        //{
+        //    //Debug.Log("RayCastDer collider " + rayCastDer.collider);
+        //    //Debug.Log("RayCastIzq collider " + rayCastIzq.collider);
+        //    plataformaChocada = rayCastIzq ? rayCastIzq.collider.gameObject : rayCastDer.collider.gameObject;
 
-            Destroy(gameObject);
-            Debug.Log("Jugador destruido");
-            GameObject newJugador = CrearJugadorEnPlataforma(transform.position);
-            MoverJugadorEnPlataforma(newJugador, lastJug.transform.position);
+        //    if(plataformaChocada.name == "Plataforma Alta")
+        //    {
+        //        currentLastJugador = LastJugadorAlta;
+
+        //    }else if (plataformaChocada.name == "Plataforma Media")
+        //    {
+        //        currentLastJugador = LastJugadorMedia;
+
+        //    }else if (plataformaChocada.name == "Plataforma Baja")
+        //    {
+        //        currentLastJugador = LastJugadorBaja;
+        //    }
+
+        //    //Destroy(gameObject);
+        //    //newJugador = CrearJugadorEnPlataforma(transform.position);
+        //    moverEnPlataforma = true;
+
+        //}
+
+        //Debug.Log(moverEnPlataforma + "Move en plataforma");
+
+        if (moverEnPlataforma)
+        {
+            MoverJugadorEnPlataforma(currentLastJugador.transform.position);
         }
     }
 
-    GameObject CrearJugadorEnPlataforma(Vector3 creationPos)
-    {
-        Debug.Log("Jugador creado");
-        GameObject jugadorEnPlat = Instantiate(JugadorPrefab, creationPos, Quaternion.identity);
-        BoxCollider2D colJugEnPlat = jugadorEnPlat.GetComponent<BoxCollider2D>();
-        colJugEnPlat.enabled = true;
-        //Rigidbody2D rbJugEnPlat = jugadorEnPlat.GetComponent<Rigidbody2D>();
-        //rbJugEnPlat.constraints = RigidbodyConstraints2D.FreezePositionY;
+    //GameObject CrearJugadorEnPlataforma(Vector3 creationPos)
+    //{
+    //    Debug.Log("Jugador creado");
+    //    GameObject jugadorEnPlat = Instantiate(JugadorPrefab, creationPos, Quaternion.identity);
+    //    BoxCollider2D colJugEnPlat = jugadorEnPlat.GetComponent<BoxCollider2D>();
+    //    colJugEnPlat.enabled = true;
+    //    //Rigidbody2D rbJugEnPlat = jugadorEnPlat.GetComponent<Rigidbody2D>();
+    //    //rbJugEnPlat.constraints = RigidbodyConstraints2D.FreezePositionY;
         
-        return jugadorEnPlat;
-    }
+    //    return jugadorEnPlat;
+    //}
 
-    void MoverJugadorEnPlataforma(GameObject jugador, Vector3 targetDir)
+    void MoverJugadorEnPlataforma(Vector3 targetDir)
     {
         //Mueve al jugador creado al chocar la plataforma hacia el Last Jugador de la plataforma chocada
-        Vector3 dir = new Vector3(targetDir.x - transform.position.x,0,0).normalized * platformMovSpeed;
-        Debug.Log("Vector dirección de movimiento: " + dir);
-        jugador.transform.Translate(dir * Time.deltaTime);
+        Vector3 dir = new Vector3(targetDir.x - transform.position.x,0,0).normalized * platformMovSpeed;  
+        transform.position = Vector3.MoveTowards(transform.position, new Vector3(targetDir.x, transform.position.y,transform.position.z), platformMovSpeed*Time.deltaTime);
+
+        Debug.Log("Current Position: " + transform.position);
+        Debug.Log("Target Position: " + new Vector3(targetDir.x, transform.position.y, transform.position.z));
+        if(transform.position == new Vector3(targetDir.x, transform.position.y, transform.position.z))
+        {
+            Debug.Log("Lo hacemos false");
+            moverEnPlataforma = false;
+            MoverLastJugador(currentLastJugador);
+        }
     }
 
-    string PlataformaColisionada(string plataforma)
+    private void MoverLastJugador(GameObject LastJugador)
     {
-        //Devuelve "Alta", "Media" o "Baja" dependiendo de en que plataforma haya aterrizado el jugador
-        string altura = plataforma.Split(' ')[1];
-        Debug.Log("Plataforma " + altura);
-        return altura;
+        LastJugador.transform.Translate(new Vector3(transform.localScale.x, 0, 0));
+        Debug.Log("Punto movido a " + LastJugador.transform.position);
     }
 
     void OnCollisionEnter2D(Collision2D collision)
@@ -84,31 +119,39 @@ public class Jugador : MonoBehaviour
         {
             Destroy(gameObject);
         }
+
+        if(collision.collider.tag == "Borde")
+        {
+            transform.position = collision.collider.gameObject.transform.position;
+        }
+
+        if(collision.collider.tag == "Plataforma")
+        {
+            //Add Jugador a lista de jugadores en plataforma
+            
+            plataformaChocada = collision.collider.gameObject;
+
+            if(plataformaChocada.name == "Plataforma Alta")
+            {
+                currentListPlat = ListJugPlatAlta;
+                currentLastJugador = plataformaChocada.transform.Find("Last Jugador Alta").gameObject;
+            }
+            else if(plataformaChocada.name == "Plataforma Media")
+            {
+                currentListPlat = ListJugPlatMedia;
+                currentLastJugador = plataformaChocada.transform.Find("Last Jugador Media").gameObject;
+            }
+            else if(plataformaChocada.name == "Plataforma Baja")
+            {
+                currentListPlat = ListJugPlatBaja;
+                currentLastJugador = plataformaChocada.transform.Find("Last Jugador Baja").gameObject;
+            }
+
+            currentListPlat.Add(gameObject);
+            
+            moverEnPlataforma = true;
+        }
     }
-
-
-
-    //private void OnTriggerEnter2D(Collider2D collision)
-    //{
-    //    if (isInPlataforma && (collision.tag == "Borde" || collision.tag == "Jugador"))
-    //    {
-    //        Mover = false;
-    //    }
-    //}
-
-    //private void OnCollisionStay2D(Collision2D collision)
-    //{
-    //    if (collision.collider.tag == "Plataforma")
-    //    {
-    //        if(seguirMoviendo && isInPlataforma)
-    //        {
-    //            Debug.Log("Sigue moviendote");
-    //            MovimientoPlataforma();
-    //        }
-
-    //    }
-    //}
-
 
 
     //void MovimientoEnPlataforma()
@@ -116,7 +159,6 @@ public class Jugador : MonoBehaviour
     //    Vector3 m_Input = new Vector3(-1, 0, 0);
     //    //rb.MovePosition(transform.position + m_Input * Time.deltaTime);
     //    transform.Translate(m_Input * Time.deltaTime);
-
 
     //    //rb.AddForce(new Vector2(- platformMovSpeed, 0));
     //    //if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
@@ -132,9 +174,6 @@ public class Jugador : MonoBehaviour
 
     //    //rb.AddForce(collision.collider.transform.right + m_Input * Time.deltaTime, ForceMode2D.Impulse);
     //}
-
-
-
 
 
 }
